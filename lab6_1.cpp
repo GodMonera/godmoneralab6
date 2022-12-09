8249a78c58be280becfd45be45a8296a6d819c0d
godmonera <borwonpak_du@cmu.ac.th>
2022-12-09 16:05:11 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,32 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x=1 , y=0 , z=0;
+
+
+    while( x != 0 ){
+
+        cout << "Enter an integer: ";
+        cin >> x ;
+
+        if(x!=0){
+
+        if(x%2==0){
+
+            y += 1 ;
+
+        }else{
+
+            z += 1 ;
+
+        }
+        }
+
+        
+    }
+    
+
+    cout << "#Even numbers = "<< y<<"\n";
+    cout << "#Odd numbers = "<< z;
     return 0;
 }
